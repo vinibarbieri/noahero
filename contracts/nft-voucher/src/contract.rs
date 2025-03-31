8647c74f365804a9ad5d957dc5b0f7569e8afff8
vinibarbieri <vinicius190702@hotmail.com>
2025-03-31 15:34:09 -0300

    feat: add-approve-nft-transfers

--- contracts/nft-voucher/src/contract.rs
+++ contracts/nft-voucher/src/contract.rs
@@ -10,6 +10,25 @@ use cw721_base::msg::InstantiateMsg as Cw721InstantiateMsg;
 use cosmwasm_std::StdError;
 
 
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn migrate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    _msg: Binary,
+) -> Result<Response, ContractError> {
+    let admin = ADMIN.load(deps.storage)?;
+    if info.sender != admin {
+        return Err(ContractError::Std(
+            cosmwasm_std::StdError::generic_err("Only admin can migrate the contract"),
+        ));
+    }
+
+    Ok(Response::new()
+        .add_attribute("action", "migrate")
+        .add_attribute("by", info.sender))
+}
+
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -106,6 +125,23 @@ pub fn execute(
                     }
                     Cw721BaseExecuteMsg::Burn { token_id }
                 },
+
+                ExecuteMsg::Approve { spender, token_id, expires } => {
+                    let treasury = TREASURY.load(deps.storage)?;
+                    let validated_spender = deps.api.addr_validate(&spender)?;
+                    if validated_spender != treasury {
+                        return Err(ContractError::Std(
+                            StdError::generic_err("Only treasury can be approved"),
+                        ));
+                    }
+
+                    Cw721BaseExecuteMsg::Approve {
+                        spender,
+                        token_id,
+                        expires,
+                    }
+                }
+                
                 _ => return Err(ContractError::Std(StdError::generic_err("Unsupported message"))),
             };
 
--- contracts/nft-voucher/src/lib.rs
+++ contracts/nft-voucher/src/lib.rs
@@ -1,6 +1,8 @@
 pub const CONTRACT_NAME: &str = "crates.io:xion-nft";
 pub const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
+pub use crate::contract::migrate;
+
 #[cfg(feature = "entry")]
 pub mod contract;
 pub mod msg;
